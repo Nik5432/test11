864c31fe3e76ff0a26648365d31d572bba7aec4f
nik54 <nik5432@lenta.ru>
2023-12-06 23:35:49 +0300

    VTB-564879 правки по ревью

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+
+    private static final String HELLO_WORLD  = "hello world!";
+
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println(HELLO_WORLD);
     }
 }
